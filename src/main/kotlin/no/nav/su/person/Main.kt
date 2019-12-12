19c04de2eeb30d387b90d564cfe1c978326fb6ab
Stefan Andreas Engelien <stefan.andreas.engelien@nav.no>
2019-12-12 10:18:43 +0100

    Walking skeleton

---
+++ src/main/kotlin/no/nav/su/person/Main.kt
@@ -0,0 +1,8 @@
+package no.nav.su.person
+
+fun main() {
+   while (true){
+      println("Hello Person!")
+      Thread.sleep(30_000)
+   }
+}
---
+++ src/test/kotlin/no/nav/su/person/MainTest.kt
@@ -0,0 +1,13 @@
+package no.nav.su.person
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+   @Test
+   fun stuff(){
+      assertTrue(true)
+   }
+
+}
