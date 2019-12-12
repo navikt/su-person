9965f26562efffa677b1676322a33f6cefb7cbae
Jacob Meidell <jacob.meidell@nav.no>
2019-12-12 15:15:39 +0100

    added logback and ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,43 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junitJupiterVersion = "5.6.0-M1"
+val ktorVersion = "1.2.5"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+   id("org.jetbrains.kotlin.jvm") version "1.3.61"
 }
 
 repositories {
-    jcenter()
+   jcenter()
+   maven("https://dl.bintray.com/kotlin/ktor")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+   implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+   implementation("ch.qos.logback:logback-classic:1.2.3")
+   implementation("net.logstash.logback:logstash-logback-encoder:5.2")
+
+   testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+   testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "12"
+   kotlinOptions.jvmTarget = "12"
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+   useJUnitPlatform()
+   testLogging {
+      events("passed", "skipped", "failed")
+   }
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.0.1"
+   gradleVersion = "6.0.1"
 }
 
 tasks.named<Jar>("jar") {
--- src/main/kotlin/no/nav/su/person/Main.kt
+++ src/main/kotlin/no/nav/su/person/Main.kt
@@ -1,8 +1,14 @@
 package no.nav.su.person
 
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.util.concurrent.TimeUnit
+
 fun main() {
-   while (true){
-      println("Hello Person!")
-      Thread.sleep(30_000)
-   }
+   val app = embeddedServer(Netty, 8080) {
+   }.start(false)
+
+   Runtime.getRuntime().addShutdownHook(Thread {
+      app.stop(5, 60, TimeUnit.SECONDS)
+   })
 }
