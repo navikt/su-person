2951e9383c32250a213a2140c6682b8389d0611a
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-12-20 12:20:46 +0100

    Init script for sourcing app specific env vars

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM navikt/java:12
 
+COPY init-scripts/* /init-scripts
 COPY build/libs/*.jar ./
 
 ENV JAVA_OPTS="-XX:MaxRAMPercentage=75 \
---
+++ init-scripts/export_app_envs.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+declare -r APP_SECRETS_FOLDER=/var/run/secrets/nais.io/vault/su-person
+[ -d "$APP_SECRETS_FOLDER" ] && {
+    source "$APP_SECRETS_FOLDER"/*.env
+    export $(cut -d= -f1 "$APP_SECRETS_FOLDER"/*.env)
+}
