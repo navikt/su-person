d871c9f6e6736edba5245d575088fb388002e127
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-12-20 12:39:29 +0100

    Change 'source' to '.' to make shell happy

--- init-scripts/export_app_envs.sh
+++ init-scripts/export_app_envs.sh
@@ -2,6 +2,6 @@
 
 APP_SECRETS_FOLDER=/var/run/secrets/nais.io/vault/su-person
 [ -d "$APP_SECRETS_FOLDER" ] && {
-    source "$APP_SECRETS_FOLDER"/*.env
+    . "$APP_SECRETS_FOLDER"/*.env
     export $(cut -d= -f1 "$APP_SECRETS_FOLDER"/*.env)
 }
